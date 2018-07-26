34a23b591bed88699eea6cdef986d6e46aee2ebb
Mark McKenna <mark.mckenna@redspace.com>
2018-07-26 13:44:04 -0300

    WIP

--- latestvaluecache/src/main/java/com/redspace/utils/latestvaluecache/LatestValueCache.kt
+++ latestvaluecache/src/main/java/com/redspace/utils/latestvaluecache/LatestValueCache.kt
@@ -1,8 +1,63 @@
 package com.redspace.utils.latestvaluecache
 
 import io.reactivex.Single
+import io.reactivex.SingleObserver
+import io.reactivex.disposables.Disposable
+import io.reactivex.disposables.Disposables
+import io.reactivex.functions.Predicate
 import io.reactivex.processors.PublishProcessor
 import io.reactivex.subjects.BehaviorSubject
+
+sealed class Cached<out V> {
+    data class Error(val e: Throwable) : Cached<Nothing>()
+    data class Value<out V>(val v: V) : Cached<V>()
+}
+
+/**
+ * @return a Single whose value is cached as long as the given predicate returns true. If the
+ * predicate returns false, the cached value is discarded and the upstream Single is triggered
+ * again.
+ * TODO: Not working yet
+ */
+fun <T> Single<T>.cacheWhile(predicate: Predicate<Cached<T>>): Single<T> {
+    val that = this
+    var cache = that.cache()
+    var value: Cached<T>? = null
+
+    return object : Single<T>() {
+        override fun subscribeActual(observer: SingleObserver<in T>) {
+            val oldValue = value
+
+            if (oldValue != null) {
+                if (predicate.test(oldValue)) {
+                    observer.onSubscribe(Disposables.empty())
+                    if (oldValue is Cached.Value<T>) observer.onSuccess(oldValue.v)
+                    else if (oldValue is Cached.Error) observer.onError(oldValue.e)
+                }
+            }
+
+            if (oldValue != null && !predicate.test(oldValue)) {
+                cache = that.cache()
+                value = null
+            }
+
+            cache.subscribe(object : SingleObserver<T> {
+                override fun onSubscribe(d: Disposable) = observer.onSubscribe(d)
+
+                override fun onSuccess(t: T) {
+                    value = Cached.Value(t)
+                    observer.onSuccess(t)
+                }
+
+                override fun onError(e: Throwable) {
+                    value = Cached.Error(e)
+                    observer.onError(e)
+                }
+            })
+        }
+    }
+}
+
 
 class LatestValueCache<V>(valueSingle: Single<V>) {
 
